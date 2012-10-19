12848bfa960057e39114a8863727e4127757d39c
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2012-10-19 19:36:58 +0200

    removing some debugging traces

--- core/src/main/java/org/lahab/clucene/core/StreamInput.java
+++ core/src/main/java/org/lahab/clucene/core/StreamInput.java
@@ -34,13 +34,10 @@ public class StreamInput extends InputStream {
 
 	@Override
 	public int read() throws IOException {
-		System.out.println("file:" + input.getFilePointer() + "/" + input.length());
 		if (input.getFilePointer() >= input.length()) {
 			return -1;
 		}
-		System.out.println("Attempt to read byte: "+ input.getFilePointer());
 		int b = (int) input.readByte() & 0xff;
-		System.out.println(b);
 		return b;
 	}
 
