ee51fbb2b7e75c25b1fdcd55dacd71f834826819
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2012-10-23 16:19:16 +0200

    adding headers for license and the automatic creation of a jar with dependencies

---
+++ client/src/main/java/org/lahab/clucene/client/SearchMain.java
@@ -0,0 +1,13 @@
+package org.lahab.clucene.client;
+
+public class SearchMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
